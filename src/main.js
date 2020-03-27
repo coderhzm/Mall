48d322ad883381027251f40fb52b4c43c054ca47
coderhzm <1915574052@qq.com>
2020-03-27 15:56:43 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -3,7 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+
+import fastclick from 'fastclick'
+
+
 Vue.config.productionTip = false
+//解决移动端点击300ms延迟
+fastclick.attach(document.body)
 
 new Vue({
   router,
---
+++ src/network/request.js
@@ -0,0 +1,33 @@
+import axios from 'axios'
+
+export function request(config){
+//1.创建axios实例
+  const instance = axios.create({
+    baseURL: 'http://123.207.32.32:8000/api/wh',
+    timeout: 5000,
+    method: 'get'
+  })
+
+//2.axios的拦截器
+  //全局拦截
+  // axios.interceptors
+  //拦截实例
+  //2.1.拦截请求
+  // instance.interceptors.request.use(config => {
+  //   console.log(config);
+  //   return config
+  // }, err => {
+  //   console.log(err);
+  // })
+  //2.2.拦截响应
+  instance.interceptors.response.use(res => {
+    // console.log(resdata);
+    return res.data
+  }, err => {
+    console.log(err);
+    
+  })
+
+//3.发送真正的网络请求
+  return instance(config)
+}
