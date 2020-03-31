58a92f37b7ba7ded0fc00b47cefb34c5fd680b1b
coderhzm <1915574052@qq.com>
2020-03-31 15:51:40 +0800

    nav-bar

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,22 @@
 <template>
   <div>
-    首页
+    <nav-bar class="home-nav"><div slot="center" >购物车</div></nav-bar>
   </div>
 </template>
 
 <script>
+import NavBar from 'components/common/navbar/NavBar'
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    NavBar
+  }
 }
 </script>
 
 <style scoped>
-
+.home-nav{
+  background-color: var(--color-high-text);
+  color: var(--color-background);
+}
 </style>
