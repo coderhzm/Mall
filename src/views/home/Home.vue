e27f748eee8f64edaae7f5a45bc7ef50cb3b087c
coderhzm <1915574052@qq.com>
2020-04-01 14:04:12 +0800

    recommend

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,19 +1,23 @@
 <template>
   <div>
     <home-nav-bar/>
-    <home-swiper :banner="banner"></home-swiper>
+    <home-swiper :banner="banner" />
+    <home-recommend :recommend="recommend" />
   </div>
 </template>
 
 <script>
 import HomeNavBar from './childComps/HomeNavBar'
 import HomeSwiper from './childComps/HomeSwiper'
+import HomeRecommend from './childComps/HomeRecommend'
+
 import {getHomeMultidata} from 'network/home'
 export default {
   name: 'Home',
   components: {
     HomeNavBar,
-    HomeSwiper
+    HomeSwiper,
+    HomeRecommend
   },
   data() {
     return {
